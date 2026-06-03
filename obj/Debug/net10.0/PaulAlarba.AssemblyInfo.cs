f01e00bacad5612c66225ad1fdf88931bcc1b7f4
Paul Alarba <alarbapaulax@gmail.com>
2026-06-03 13:50:26 +0800

    Update: About me page

--- obj/Debug/net10.0/PaulAlarba.AssemblyInfo.cs
+++ obj/Debug/net10.0/PaulAlarba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaulAlarba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b23f1e24de2b47662ff51d22b7c398f7470c2d4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d80d8912feedcb468cdefe063e151dd039995f69")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaulAlarba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaulAlarba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
