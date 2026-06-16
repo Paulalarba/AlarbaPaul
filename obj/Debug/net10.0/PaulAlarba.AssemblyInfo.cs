2e39a6b3b726f2b100ed654017f43c428d93baec
Paul Alarba <alarbapaulax@gmail.com>
2026-06-16 09:40:54 +0800

    Update: remove the text in message section

--- obj/Debug/net10.0/PaulAlarba.AssemblyInfo.cs
+++ obj/Debug/net10.0/PaulAlarba.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaulAlarba")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7311d47b731a6e31b5244eca8ce9702cf0d526fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc2e2e7f3cd11731abd1dfb3de466113c82c69e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaulAlarba")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaulAlarba")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
